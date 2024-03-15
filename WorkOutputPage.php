fce55d2ba6c59f6b9d1e28d0f773197c2e527f20
alex323237777 <163555951+alex323237777@users.noreply.github.com>
2024-03-15 16:33:06 +0000

    main

--- WorkOutputPage.php
+++ WorkOutputPage.php
@@ -6,26 +6,6 @@
     <title>Work Ouput Page</title>
 </head>
 <body>
-<table style="width:100%">
-  <tr>
-    <th>Sections & Items</th>
-    <th>Details/Quantity</th>
-    <th>Score Range</th>
-  </tr>
-  <tr>
-    <td>Section A</td>
-    <td>1</td>
-    <td>2</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico and italy</td>
-  </tr>
+    yt5656
 </body>
 </html>
