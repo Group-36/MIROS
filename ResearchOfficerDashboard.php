aac6b3fc9c1efba4c53280607b20914390ee48b4
Ibi101 <147090950+Ibi101@users.noreply.github.com>
2024-03-19 03:19:20 +0000

    Update ResearchOfficerDashboard.php

--- ResearchOfficerDashboard.php
+++ ResearchOfficerDashboard.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Company Logo and World Clocks</title>
+    <title>CResearch Officer Dashboard</title>
     <style>
         body {
             margin: 0;
