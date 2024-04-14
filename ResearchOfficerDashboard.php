330f078a3a72f89b92d630ffdf8a32018e423922
DanteSHU <163324392+DanteSHU@users.noreply.github.com>
2024-04-14 20:24:26 +0100

    Add files via upload

--- ResearchOfficerDashboard.php
+++ ResearchOfficerDashboard.php
@@ -1,116 +1,148 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>CResearch Officer Dashboard</title>
-    <style>
- body {
-     margin: 0;
-      padding: 0;
-      background-color: #fff;
-  font-family: Arial, sans-serif; }
-  #logo {
-  position: absolute;
-  top: 5px;
-  left: 10px;
-  width: 250px;
-      height: auto;   }
-        #clocks {
-  position: absolute;
-  top: 125px;
-  left: 600px;
-  text-align: center;
-  font-size: 20px;   } 
-     #London {
-  position: absolute;
-  top: 5px;
-  left: 860px;  }
-      #Tokyo {
- position: absolute;
- top: 5px;
-  left: 1060px;   }
-        #KualaLumpur {
- position: absolute;
- top: 5px;
- left: 1260px;   }  
-        .clock {
-  display: inline-block;
-   margin: 0 20px;
-      }
-     #NewYork{
-         position:absolute;
-         top: 5px;
-         left: 650px;
-     }
-     #Dubai{
-         position: absolute;
-         top: 5px;
-         left: 1500px;
-     }
-      #notificationPanel{
-         position: fixed;
-         left: 0;
-         top: 0;
-         width: 150px;
-         height: 100%;
-         background-color: #C0C0C0;
-         padding: 20px;
-         
-     }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Research Officer Dashboard</title>
+    <link rel="stylesheet" href="miros.css">
 </head>
 <body>
-    <img id="logo" src= "images/MIROS_logo(1).png" alt="Logo">
+  <div class="container">
+      <img src="https://img02.mockplus.com/rp/image/2024-03-07/537e3a50-dbe1-11ee-9ed8-7f6f33dbdfc3.png" alt="Logo" class="logo">
+      <div class="user-info">
+      <header style="margin-right: 16vw;">
+          <h1 style="color: rgb(27, 131, 129); font-weight: bold;">Research Officer Dashboard</h1>
+          </header>
+        <button class="language-button bm" style="font-weight:bold; font-size:20px;" onclick="window.location.href='WorkOutputPageM.php'" >bm</button>
+         <button class="language-button en" style="font-weight:bold; font-size:20px;" onclick="window.location.href='WorkOutputPage.php'">en</button>
+          <button style="background-color: blue; font-weight:bold; font-size:20px;">Log Out</button>
+          <img src="https://www.svgrepo.com/show/497407/profile-circle.svg" alt="User Profile" width="55">
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+      <form id="myForm">
+        <div class="form-group">
+        
+<div class="charts">
+      <div class="charts" style="margin-left: 12vw;">
+<table>
+  <tr>
+    <th>
+      <div class="charts-card">
+        <h1 class="chart-title">Record Activity</h1>
+        <form id="myForm">
+        <div class="form-group">
+            <label for="sectionid" style="color: black;">Please Select Activity Type</label>
+            <select class="sectionclass" id="sectionid" onchange="updateItems()">
+              <option value="A">Research Publication</option>
+              <option value="B">Consultation Project </option>
+              <option value="C">Operational Involvement</option>
+            </select>
+</div>
+
+      <div class="form-group">
+          <label for="DQ" style="color: black;">Details</label>
+          <input type="text" class="DQclass" id="DQid" placeholder="Details" required>
+      </div>
+
+      <div style="text-align: left;">
+        <label for="file" style="color: black;">Please Choose File</label>
+        <input type="file" class="addfile" id="addfileid" placeholder="Please Choose File" style="color: black;" required>
+      </div>
+      <br>
+      
+      <button type="submit" id="submit-btn">Submit</button>
+    </form>
+  </div>
+    </th>
     
-    <div id="clocks">
-    <div class="clock" id="new-york-clock"></div>
-
-   <div class="clock" id="london-clock"></div>
-
-        <div class="clock" id="tokyo-clock"></div>
-
-
-  <div class="clock" id="kuala-lumpur-clock"></div>
-
-        <div class="clock" id="dubai-clock"></div>
-    </div>
-
-    <iframe id="London" src="https://free.timeanddate.com/clock/i9aho7r3/n1316/szw110/szh110/hocddd/hbw0/hfc000/cf100/hgr0/hwc000/fav0/fiv0/mqcfff/mql15/mqw8/mqd100/mhcfff/mhl15/mhw4/mhd100/mmv0/hhcff9/hmcff9" frameborder="0" width="110" height="110"></iframe>
- 
- 
-    <iframe id="Tokyo" src="https://free.timeanddate.com/clock/i9aho7r3/n248/szw110/szh110/hocddd/hbw0/hfc000/cf100/hgr0/hwc000/fav0/fiv0/mqcfff/mql15/mqw8/mqd100/mhcfff/mhl15/mhw4/mhd100/mmv0/hhcff9/hmcff9" frameborder="0" width="110" height="110"></iframe>
-   
-   
-    <iframe id="KualaLumpur" src="https://free.timeanddate.com/clock/i9aho7r3/n4826/szw110/szh110/hocddd/hbw0/hfc000/cf100/hgr0/hwc000/fav0/fiv0/mqcfff/mql15/mqw8/mqd100/mhcfff/mhl15/mhw4/mhd100/mmv0/hhcff9/hmcff9" frameborder="0" width="110" height="110"></iframe>
-  
-    <iframe id = "NewYork" src="https://free.timeanddate.com/clock/i9btdla8/n4826/szw110/szh110/hocddd/hbw0/hfc000/cf100/hgr0/hwc000/fav0/fiv0/mqcfff/mql15/mqw8/mqd100/mhcfff/mhl15/mhw4/mhd100/mmv0/hhcff9/hmcff9" frameborder="0" width="110" height="110"></iframe>
+    <th>
+      <div class="charts-card" style="margin-left: 30px;">
+        <p class="chart-title">Workoutput</p>
+        <dl>
+        <button type="submit" id="submit-btn" onclick="window.location.href='WorkOutputPage.php'">Set Workoutput</button>
+        </dl>
+        <br>
+        <p class="chart-title">Work Targets & KPI's</p>
+        <dl>
+        <button type="submit" id="submit-btn" onclick="window.location.href='WorkTKPI.php'">Set Work Target & KPI</button>
+        </dl>
+      </div>
+    </th>
     
-    <iframe id = "Dubai" src = "https://free.timeanddate.com/clock/i9bten57/n776/szw110/szh110/hocddd/hbw0/hfc000/cf100/hgr0/hwc000/fav0/fiv0/mqcfff/mql15/mqw8/mqd100/mhcfff/mhl15/mhw4/mhd100/mmv0/hhcff9/hmcff9" frameborder="0" width="110" height="110"></iframe>
+    <th>
+      <div class="charts-card" style="margin-left: 30px;">
+        <p class="chart-title">Notifications</p>
+        <dl>
+          <dd style="text-align: left;">- Notification 1</dd>
+          <dd style="text-align: left;">- Notification 2</dd>
+          <dd style="text-align: left;">- Notification 3</dd>
+          </dl>
+      </div>
+    </th>
+  </tr>
+
+  <tr>
+  <th>
+      <div class="charts-card">
+        <p class="chart-title">Tasks</p>
+        <dl>
+          <dd style="text-align: left;">- Task 1</dd>
+          <dd style="text-align: left;">- Task 2</dd>
+          <dd style="text-align: left;">- Task 3</dd>
+          </dl>
+  </div>
+    </th>
+
+    <th>
+      <div class="charts-card" style="margin-left: 30px;">
+        <p class="chart-title">Progress</p>
+        <div id="donut-chart"></div>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawVisualization);
+
+      function drawVisualization() {
+        // Some raw data (not necessarily accurate)
+        var data = google.visualization.arrayToDataTable([
+          ['Activity', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+          ['Workoutput & KPI',  165,      938,         522,             998,           450,      614.6],
+          ['Reserach Publication',  135,      1120,        599,             1268,          288,      682],
+          ['Consultation Project',  157,      1167,        587,             807,           397,      623],
+          ['Operational Involvement',  139,      1110,        615,             968,           215,      609.4],
+        ]);
+
+        var options = {
+          title : 'Progress',
+          vAxis: {title: 'Completed'},
+          hAxis: {title: 'Activties'},
+          seriesType: 'bars',
+          series: {5: {type: 'line'}}
+        };
+
+        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+        <div id="chart_div" style="width: auto; height: 25vh;"></div>
+      </div>
+      </div>
+    </th>
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-   
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.36/moment-timezone-with-data.min.js"></script>
-<script>
-        function updateClocks() {
-  const newYorkTime = moment().tz("America/New_York").format('HH:mm:ss');
-            document.getElementById('new-york-clock').innerText = `New York: ${newYorkTime}`;
-
-          
-          
-  const londonTime = moment().tz("Europe/London").format('HH:mm:ss');
-      document.getElementById('london-clock').innerText = `London: ${londonTime}`;
-
-  const tokyoTime = moment().tz("Asia/Tokyo").format('HH:mm:ss');
-      document.getElementById('tokyo-clock').innerText = `Tokyo: ${tokyoTime}`;
-
-     const kualaLumpurTime = moment().tz("Asia/Kuala_Lumpur").format('HH:mm:ss');
-   document.getElementById('kuala-lumpur-clock').innerText = `Kuala Lumpur: ${kualaLumpurTime}`;
-
-            const dubaiTime = moment().tz("Asia/Dubai").format('HH:mm:ss');
-  document.getElementById('dubai-clock').innerText = `Dubai: ${dubaiTime}`;
-        }
-
-        setInterval(updateClocks, 1000);
-        updateClocks();
-</script>
- <div id = "notificationPanel"> <h2> Tasks</h2> <ul> <li> Task 1</li> <li> Task 2</li> <li> Task 3</li> </ul> </div>
-</body>
-</html>
+    <th>
+      <div class="charts-card" style="margin-left: 30px;">
+        <p class="chart-title">Deadlines</p>
+        <dl>
+          <dd style="text-align: left;">- Deadline 1</dd>
+          <dd style="text-align: left;">- Deadline 2</dd>
+          <dd style="text-align: left;">- Deadline 3</dd>
+          </dl>
+        <div id="reminders-warnings-chart"></div>
+      </div>
+    </th>
+</tr>
+</table>
+</div>
