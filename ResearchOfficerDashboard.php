3c3bd42d3f09683868a297889551b1b29368a311
DanteSHU <163324392+DanteSHU@users.noreply.github.com>
2024-04-19 16:32:25 +0100

    Update ResearchOfficerDashboard.php

--- ResearchOfficerDashboard.php
+++ ResearchOfficerDashboard.php
@@ -15,7 +15,7 @@
           </header>
         <button class="language-button bm" style="font-weight:bold; font-size:20px;" onclick="window.location.href='ResearchOfficerDashboardM.php'" >bm</button>
          <button class="language-button en" style="font-weight:bold; font-size:20px;" onclick="window.location.href='ResearchOfficerDashboard.php'">en</button>
-          <button style="background-color: blue; font-weight:bold; font-size:20px;">Log Out</button>
+          <button style="background-color: blue; font-weight:bold; font-size:20px;" onclick="window.location.href='logout.php'">Log Out</button>
           <img src="https://www.svgrepo.com/show/497407/profile-circle.svg" alt="User Profile" width="55">
       </div>
       <br>
