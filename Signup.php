14cdf4c54d0af77f6eb2f9a73026473ca392e114
alex323237777 <163555951+alex323237777@users.noreply.github.com>
2024-04-24 01:52:04 +0100

    Update Signup.php
    adding functionality

--- Signup.php
+++ Signup.php
@@ -1,63 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up Page</title>
-    <link rel="stylesheet" href="miros.css">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="signupcss.css">
 </head>
-
-<body class="body2">
-    <div class="white-border" style="text-align: center;">
-        <div style="transition: unset; inset: 0px; text-align: left; margin-bottom: -160px;"><img width="367" height="124" src="https://img02.mockplus.com/rp/image/2024-03-07/537e3a50-dbe1-11ee-9ed8-7f6f33dbdfc3.png"></div>
-        <h1 style="color: black; font-weight: bold; text-align: center; font-size: 40px;">Create Account</h1>
-        <div style="transition: unset; inset: 0px; text-align: center;"><img width="405" height="246" src="https://img02.mockplus.com/rp/image/2024-03-14/90d76c00-e159-11ee-8f72-115981801776.png" class="headerborder"></div>
-        <hr>
-        <br>
-
-        <form style="display: flex; justify-content: space-between;">
-            <div style="flex: 0 0 calc(50%);">
-                <div class="form-group1">
-                    <input type="text" class="form-control" name="First_Name" placeholder=" First Name" style="height: 45px; width: 600px; border-radius: 20px;" required>
-                </div>
-                <br>
-                <br>
-
-                <div class="form-group1">
-                    <input type="text" class="form-control" name="Last_Name" placeholder=" Last Name" style="height: 45px; width: 600px; border-radius: 20px;" required>
-                </div>
-                <br>
-                <br>
-
-                <div class="form-group1">
-                    <input type="date" class="form-control" name="DOB" id="dob-input" style="height: 45px; width: 605px; border-radius: 20px;" required>
-                    <label for="dob-input" class="placeholder" style="margin-top: 10px; color: black;">Date Of Birth</label>
-                </div>
-            </div>
-
-            <div style="flex: 0 0 calc(50%);">
-                <div class="form-group1">
-                    <input type="email" class="form-control" name="Email" placeholder=" Email" style="height: 45px; width: 600px; border-radius: 20px;" required>
-                </div>
-                <br>
-                <br>
-
-                <div class="form-group1">
-                    <input type="password" class="form-control" name="Password" placeholder=" Password" style="height: 45px; width: 600px; border-radius: 20px;" required>
-                </div>
-                <br>
-                <br>
-
-                <div class="form-group1">
-                    <input type="password" class="form-control" name="Confirm_Password" placeholder=" Confirm Password" style="height: 45px; width: 600px; border-radius: 20px;" required>
-                </div>
-
-                <div class="checkbox" style="margin-left: -900px; margin-top: 75px;">
-                    <input type="checkbox" id="terms_and_condition" style="text-align: auto;"> <!--margin-left: -215px;-->
-                    <label for="I agree to the terms and conditions" style="font-size: 20px; color:black; text-align: auto;" required>I agree to the terms and conditions</label><br> <!--margin-right: -150px; margin-top: -20px;-->
-                    <button id="myButton" class="btn btn-primary" style="background-color: rgb(249, 99, 50);; color: white; border-radius: 20px; width: 130px; height: 50px; text-align: center;" onclick="window.location.href='login.php'">Get Started</button>
-                </div>
-        </form>
+<body>
+    <img src="miroslogo.png" alt="Miros Logo" class="logo">
+    <div class="signup-form">
+        <h2 class="signup-title">Sign Up</h2>
+        <form action="signup_process.php" method="post">
+        <div class="input-container">
+            <label for="signup_fn">First Name:</label>
+            <input type="text" id="signup_fn" name="signup_fn">
+        </div>
+        <div class="input-container">
+            <label for="signup_ln">Last Name:</label>
+            <input type="text" id="signup_ln" name="signup_ln">
+        </div>
+        <div class="input-container">
+            <label for="signup_DOB">Date of Birth:</label>
+            <input type="date" id="signup_DOB" name="signup_DOB">
+        </div>
+        <div class="input-container">
+            <label for="signup_email">Email:</label>
+            <input type="email" id="signup_email" name="signup_email">
+        </div>
+        <div class="input-container">
+            <label for="signup_password">Password:</label>
+            <input type="password" id="signup_password" name="signup_password">
+        </div>
+        <button class="get-started-btn" type="submit">Get Started</button>
     </div>
 </body>
+</html>
